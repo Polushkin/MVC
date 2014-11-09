47d1797c1b4a52d18b3726454b03efea4a61f0ff
Polushkin <ccfytrr@mail.ru>
2014-11-09 19:04:05 +0400

    Сделал методы статическими

--- src/mvc/View.java
+++ src/mvc/View.java
@@ -1,5 +1,20 @@
 package mvc;
 
 public class View {
-    
+    public static void updateSView(String name, String faculty,
+                            int groupNumber, String date){
+        System.out.println("---------------------------------------------------");
+        System.out.println("Student's Name"+" | "+"Faculty"+" | "+"Group"+" | "+"DateOfEnrollment");
+        printDetails(name, faculty,groupNumber,date);
+    }
+    public static void updateGView(){
+        System.out.println("---------------------------------------------------");
+        System.out.println("Student's Name"+" | "+"Faculty"+" | "+"Group"+" | "+"DateOfEnrollment");
+        System.out.println("---------------------------------------------------");
+    }
+    public static void printDetails(String name, String faculty,
+                             int groupNumber, String date){
+        System.out.println(name+" | "+faculty+" | "+groupNumber+" | "+date);
+        System.out.println("---------------------------------------------------");
+    }
 }
