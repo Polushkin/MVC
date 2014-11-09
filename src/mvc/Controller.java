69d307ccc952e37b0950ffc2ec11af7085c41020
Polushkin <ccfytrr@mail.ru>
2014-11-09 17:53:28 +0400

    1

--- src/mvc/Controller.java
+++ src/mvc/Controller.java
@@ -1,5 +1,20 @@
 package mvc;
 
+
 public class Controller {
-    
+
+    String command;
+
+    public Controller(String reader) {
+        this.command = reader;
+    }
+
+    public boolean analysisCommand() {
+        String strings[];   //1 команда  2 параметр 3 остаток от строки
+        strings = command.split(" ", 3);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(strings[i]);
+        }
+        return false;
+    }
 }
--- src/mvc/MVC.java
+++ src/mvc/MVC.java
@@ -1,9 +1,20 @@
 package mvc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MVC {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        boolean isExit;
+        do {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            Controller controller = new Controller(reader.readLine());
+            isExit = controller.analysisCommand();
+        }
+        while (isExit);
     }
 
 }
