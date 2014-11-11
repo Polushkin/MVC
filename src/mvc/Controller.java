364975e830d235c82ca03c84c4313c60cb4215b8
Polushkin <ccfytrr@mail.ru>
2014-11-11 21:56:35 +0400

    Controller

--- src/mvc/Controller.java
+++ src/mvc/Controller.java
@@ -11,10 +11,58 @@ public class Controller {
 
     public boolean analysisCommand() {
         String strings[];   //1 команда  2 параметр 3 остаток от строки
-        strings = command.split(" ", 3);
-        for (int i = 0; i < 3; i++) {
-            System.out.println(strings[i]);
+        strings = command.split(" ", 2);
+        switch (strings[0]) {
+            case "addstunent":
+                break;
+            case "addgroup":
+                break;
+            case "getstudent":
+                break;
+            case "getgroup":
+                break;
+            case "setstudent":
+                break;
+            case "setgroup":
+                break;
+            case "delstudent":
+                break;
+            case "delgroup":
+                break;
+
         }
         return false;
     }
+
+    private boolean addStudent(String s[]){
+
+    }
+
+    private boolean getStudent(String s[]){
+
+    }
+
+    private boolean delStudent(String s[]){
+
+    }
+
+    private boolean setStudent(String s[]){
+
+    }
+
+    private boolean addGroup(String s[]){
+
+    }
+
+    private boolean getGroup(String s[]){
+
+    }
+    private boolean setStudent(String s[]){
+
+    }
+
+    private boolean delStudent(String s[]){
+
+    }
+
 }
