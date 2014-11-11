4474e677c23ee3f6ad1469414420cc0e1900e3b6
Polushkin <ccfytrr@mail.ru>
2014-11-11 21:07:10 +0400

    Чуть изменил сериализацию.

--- src/mvc/GroupDaoSerial.java
+++ src/mvc/GroupDaoSerial.java
@@ -1,19 +1,23 @@
 package mvc;
 
+
 import java.io.*;
 import java.util.ArrayList;
 
-public class GroupDaoSerial{
+public class GroupDaoSerial {
 
-    public static void writeGroup(Object o) throws IOException {
+    public static void writeGroup(ArrayList<Group> groups) throws IOException {
         ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("Group"));
-        outputStream.writeObject(o);
+        outputStream.writeObject(groups);
+        outputStream.flush();
+        outputStream.close();
     }
 
-    public static Object readGroup() throws IOException, ClassNotFoundException {
+    public static ArrayList<Group> readGroup() throws IOException, ClassNotFoundException {
         ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("Group"));
-        Object o = inputStream.readObject();
-        return o;
+        ArrayList<Group> groups = (ArrayList<Group>) inputStream.readObject();
+        inputStream.close();
+        return groups;
     }
 
 
--- src/mvc/StudentDaoSerial.java
+++ src/mvc/StudentDaoSerial.java
@@ -2,18 +2,22 @@ package mvc;
 
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class StudentDaoSerial {
 
-    public static void writeStudents(Object o) throws IOException {
+    public static void writeStudents(ArrayList<Student> students) throws IOException {
         ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("students"));
-        outputStream.writeObject(o);
+        outputStream.writeObject(students);
+        outputStream.flush();
+        outputStream.close();
     }
 
-    public static Object readStudents() throws IOException, ClassNotFoundException {
+    public static ArrayList<Student> readStudents() throws IOException, ClassNotFoundException {
         ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("students"));
-        Object o = inputStream.readObject();
-        return o;
+        ArrayList<Student> students = (ArrayList<Student>) inputStream.readObject();
+        inputStream.close();
+        return students;
     }
 
 }
