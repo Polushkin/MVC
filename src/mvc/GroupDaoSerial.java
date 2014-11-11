465e56ff88fa56f835befc244480bd37a0885fe9
Polushkin <ccfytrr@mail.ru>
2014-11-11 20:58:35 +0400

    Добавил сериализацию.

--- src/mvc/GroupDaoSerial.java
+++ src/mvc/GroupDaoSerial.java
@@ -1,37 +1,20 @@
 package mvc;
 
+import java.io.*;
 import java.util.ArrayList;
 
-public class GroupDaoSerial implements GroupDao{
+public class GroupDaoSerial{
 
-    @Override
-    public void addGroup(int groupNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static void writeGroup(Object o) throws IOException {
+        ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("Group"));
+        outputStream.writeObject(o);
     }
 
-    @Override
-    public void addGroup(int groupNumber, String faculty) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static Object readGroup() throws IOException, ClassNotFoundException {
+        ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("Group"));
+        Object o = inputStream.readObject();
+        return o;
     }
 
-    @Override
-    public void removeGroup(int groupNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
-    @Override
-    public Group getGroupByNumber(int groupNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ArrayList getGroupsByFaculty(String faculty) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ArrayList getAllGroups() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
--- src/mvc/StudentDaoSerial.java
+++ src/mvc/StudentDaoSerial.java
@@ -1,32 +1,19 @@
 package mvc;
 
-import java.util.ArrayList;
 
-public class StudentDaoSerial implements StudentDao {
+import java.io.*;
 
-    @Override
-    public void addStudent(String name, String dateOfEnrollment, Group groupOfStudent, int idNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+public class StudentDaoSerial {
+
+    public static void writeStudents(Object o) throws IOException {
+        ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("students"));
+        outputStream.writeObject(o);
     }
 
-    @Override
-    public void removeStudent(int idNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Student getStudentById(int idNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ArrayList getAllStudents() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ArrayList getStudentsByGroup(int groupNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static Object readStudents() throws IOException, ClassNotFoundException {
+        ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("students"));
+        Object o = inputStream.readObject();
+        return o;
     }
 
 }
